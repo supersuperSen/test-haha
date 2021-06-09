c111daa9c7c09b0da638b0d456b163769aca303e
lisen <1536940785@qq.com>
2021-06-09 14:21:28 +0800

    init

---
+++ src/main/java/com/example/testhaha/Test.java
@@ -0,0 +1,7 @@
+package com.example.testhaha;
+
+public class Test {
+    public static void main(String[] args) {
+        System.err.println("hahah");
+    }
+}
---
+++ src/main/java/com/example/testhaha/TestHahaApplication.java
@@ -0,0 +1,13 @@
+package com.example.testhaha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHahaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHahaApplication.class, args);
+    }
+
+}
